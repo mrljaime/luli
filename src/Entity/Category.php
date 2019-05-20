7f176a3682c62dc1c2355c7389440d13bcf11da9
José Jaime Ramírez Calvo <mr.ljaime@gmail.com>
2019-05-20 11:33:49 -0500

    Implementación para serializador json

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -14,7 +14,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
  * @ORM\Table(name="aa_categories")
  */
-class Category
+class Category implements \JsonSerializable
 {
     /**
      * @ORM\Id()
@@ -43,5 +43,20 @@ class Category
         $this->name = $name;
 
         return $this;
+    }
+
+    /**
+     * Specify data which should be serialized to JSON
+     * @link https://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4.0
+     */
+    public function jsonSerialize()
+    {
+        return [
+            'id'    => $this->id,
+            'name'  => $this->name,
+        ];
     }
 }
--- src/Entity/SubCategory.php
+++ src/Entity/SubCategory.php
@@ -8,7 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity(repositoryClass="App\Repository\SubCategoryRepository")
  * @ORM\Table(name="aa_sub_categories")
  */
-class SubCategory
+class SubCategory implements \JsonSerializable
 {
     /**
      * @ORM\Id()
@@ -57,5 +57,23 @@ class SubCategory
         $this->name = $name;
 
         return $this;
+    }
+
+    /**
+     * Specify data which should be serialized to JSON
+     * @link https://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4.0
+     */
+    public function jsonSerialize()
+    {
+        return [
+            'id'        => $this->id,
+            'name'      => $this->name,
+            'category'  => [
+                'id'    => $this->category->getId(),
+            ]
+        ];
     }
 }
