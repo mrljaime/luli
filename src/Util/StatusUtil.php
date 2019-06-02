4452c25b09d442cb380546ee1180403e18f01f9c
José Jaime Ramírez Calvo <mr.ljaime@gmail.com>
2019-06-02 11:55:25 -0500

    Allow use bitwise with status

---
+++ src/Util/StatusUtil.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @author <a href="mailto:mr.ljaime@gmail.com">José Jaime Ramírez Calvo</a>
+ * @version 1
+ * @since 2019-05-26
+ */
+
+namespace App\Util;
+
+/**
+ * Class StatusUtil
+ * @package App\Util
+ */
+class StatusUtil
+{
+    const PENDING = 0x1;
+
+    const PAID = 0x2;
+
+    const SENT = 0x4;
+
+    /**
+     * @param $pending
+     * @return bool
+     */
+    public static function isPending($pending)
+    {
+        return (0 != (self::PENDING & $pending));
+    }
+
+    /**
+     * @param $paid
+     * @return bool
+     */
+    public static function isPaid($paid)
+    {
+        return (0 != (self::PAID & $paid));
+    }
+
+    /**
+     * @param $sent
+     * @return bool
+     */
+    public static function isSent($sent)
+    {
+        return (0 != (self::SENT & $sent));
+    }
+}
