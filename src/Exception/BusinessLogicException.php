29de293c326a91e8c3f725d38061ba9c2a566d24
José Jaime Ramírez Calvo <mr.ljaime@gmail.com>
2019-06-02 22:19:56 -0500

    BusinessLogicException

---
+++ src/Exception/BusinessLogicException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author José Jaime Ramírez Calvo <mr.ljaime@gmail.com>
+ * @version 1
+ * @since 1
+ */
+
+namespace App\Exception;
+
+/**
+ * Class BusinessLogicException
+ * @package App\Exception
+ */
+class BusinessLogicException extends \Exception
+{
+
+}
